ea6c3253722b9a67bdeb6049dd7dd14c4fc47054
Ahmed Haitham <96428026+Sian1902@users.noreply.github.com>
2026-07-08 03:15:14 +0300

    feature confirmation done

--- app/src/main/java/com/example/qafilah/core/currency/di/CurrencyModule.kt
+++ app/src/main/java/com/example/qafilah/core/currency/di/CurrencyModule.kt
@@ -5,6 +5,7 @@ import com.example.qafilah.core.currency.data.remote.CurrencyService
 import com.example.qafilah.core.currency.domain.repo.CurrencyRepository
 import com.example.qafilah.core.currency.domain.repo.CurrencyRepositoryImpl
 import com.example.qafilah.core.currency.domain.usecase.ConvertPriceUseCase
+import com.example.qafilah.core.currency.domain.usecase.ConvertRawPriceUseCase
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -21,4 +22,5 @@ val currencyModule = module {
     single<CurrencyRepository> { CurrencyRepositoryImpl(get(), get()) }
 
     factory { ConvertPriceUseCase(get()) }
+    factory { ConvertRawPriceUseCase(get()) }
 }
